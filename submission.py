e447445ebd60a01a169aa1ef13fc97c047d36cf6
roeygraif <118445784+roeygraif@users.noreply.github.com>
2024-07-28 18:04:07 +0300

    working

--- submission.py
+++ submission.py
@@ -3,6 +3,7 @@ from WarehouseEnv import WarehouseEnv, manhattan_distance
 import random
 import time
 import numpy as np
+from func_timeout import func_timeout, FunctionTimedOut
 
 # TODO: section a : 3
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
@@ -30,46 +31,190 @@ class AgentGreedyImproved(AgentGreedy):
         return smart_heuristic(env, robot_id)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class AgentMinimax(Agent):
     # TODO: section b : 1
+    
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        start_time = time.time()
-        tree_height = 0
-        child_ops, children_env = self.successors(env,agent_id)
-        child_list = []
-        while time.time() < start_time + time_limit - 0.7:
-            child_list = [self.mini_max_RB(child_env, agent_id, tree_height, (agent_id+1)%2) for child_env in children_env]
-            print(child_list)
-            tree_height = tree_height + 1
-        if child_list is []:
-            return None
-        return child_ops[child_list.index(max(child_list))]
+        curr_best = None
+        
+        def run_step_timeout():
+            nonlocal curr_best
+            tree_height = 0
+            child_list = []
+            child_ops, children_env = self.successors(env,agent_id)
+            while True:
+                child_list = [self.mini_max_RB(child_env, agent_id, tree_height, (agent_id+1)%2) for child_env in children_env]
+                current_max = max(child_list)
+                current_max_index = child_list.index(current_max)
+                curr_best = child_ops[current_max_index]
+                tree_height = tree_height + 1
+
+        try:
+            func_timeout(time_limit * 0.8, run_step_timeout, args=()) 
+        except FunctionTimedOut:
+            pass
+        return curr_best
     
     def mini_max_RB(self, env: WarehouseEnv, agent_id, tree_height, turn):
-        if tree_height == 0 or env.done():
-            return smart_heuristic(env, agent_id)
-        child_ops, children_env = self.successors(env, agent_id)
-        for child_env in children_env:
-            if turn is agent_id:
-                ret_val = -1 * np.inf
+        if env.done() or tree_height == 0:
+            return smart_heuristic(env,agent_id)
+        _ ,children_env = self.successors(env,turn)
+        if turn == agent_id:
+            current_max = (-1)*np.inf
+            for child_env in children_env:
                 val = self.mini_max_RB(child_env, agent_id, tree_height - 1, (turn+1)%2)
-                ret_val = max(val, ret_val)
-            else:
-                ret_val = np.inf
+                current_max = max(current_max,val)
+            return current_max
+        else:
+            current_min = np.inf
+            for child_env in children_env:
                 val = self.mini_max_RB(child_env, agent_id, tree_height - 1, (turn+1)%2)
-                ret_val = min(val, ret_val)
-        return ret_val
+                current_min = min(current_min,val)
+            return current_min
+
+
+
+
+
+
+
+
+
+
 
 class AgentAlphaBeta(Agent):
     # TODO: section c : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        curr_best = None
+        
+        def run_step_timeout():
+            nonlocal curr_best
+            tree_height = 0
+            child_list = []
+            child_ops, children_env = self.successors(env,agent_id)
+            while True:
+                a = -np.inf
+                b = np.inf
+                child_list = [self.RB_alpha_beta(child_env, agent_id, tree_height, (agent_id+1)%2, a, b) for child_env in children_env]
+                current_max = max(child_list)
+                current_max_index = child_list.index(current_max)
+                curr_best = child_ops[current_max_index]
+                tree_height = tree_height + 1
+
+        try:
+            func_timeout(time_limit * 0.8, run_step_timeout, args=()) 
+        except FunctionTimedOut:
+            pass
+        return curr_best
+    
+    def RB_alpha_beta(self, env: WarehouseEnv, agent_id, tree_height, turn, a, b):
+        if env.done() or tree_height == 0:
+            return smart_heuristic(env,agent_id)
+        _ ,children_env = self.successors(env,turn)
+        if turn == agent_id:
+            current_max = (-1)*np.inf
+            for child_env in children_env:
+                val = self.RB_alpha_beta(child_env, agent_id, tree_height - 1, (turn+1)%2, a, b)
+                current_max = max(current_max,val)
+                a = max(current_max, a)
+                if current_max >= b:
+                    return np.inf
+            return current_max
+        else:
+            current_min = np.inf
+            for child_env in children_env:
+                val = self.RB_alpha_beta(child_env, agent_id, tree_height - 1, (turn+1)%2, a, b)
+                current_min = min(current_min,val)
+                b = min(current_min, b)
+                if current_min <= a:
+                    return (-1)*np.inf
+            return current_min
+
+
+
+
+
+
+
+
+
 
 
 class AgentExpectimax(Agent):
     # TODO: section d : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        curr_best = None
+        
+        def run_step_timeout():
+            nonlocal curr_best
+            tree_height = 0
+            child_list = []
+            child_ops, children_env = self.successors(env,agent_id)
+            while True:
+                child_list = [self.RB_expectimax(child_env, agent_id, tree_height, (agent_id+1)%2) for child_env in children_env]
+                current_max = max(child_list)
+                current_max_index = child_list.index(current_max)
+                curr_best = child_ops[current_max_index]
+                tree_height = tree_height + 1
+
+        try:
+            func_timeout(time_limit * 0.8, run_step_timeout, args=()) 
+        except FunctionTimedOut:
+            pass
+        return curr_best
+
+    def RB_expectimax(self, env: WarehouseEnv, agent_id, tree_height, turn):
+        if env.done() or tree_height == 0:
+            return smart_heuristic(env,agent_id)
+        child_ops ,children_env = self.successors(env,turn)
+        if turn == agent_id:
+            current_max = (-1)*np.inf
+            for child_env in children_env:
+                val = self.RB_expectimax(child_env, agent_id, tree_height - 1, (turn+1)%2)
+                current_max = max(current_max,val)
+            return current_max
+        else:
+            num_ops = len(child_ops)
+            sum_val = 0
+            val = 0
+            for op, child_env in zip(child_ops, children_env):
+                if ("move east" in child_ops) and ("pick up" in child_ops):
+                    if (op is "move east") or (op is "pick up"):
+                        val = (2/(num_ops+2)) * self.RB_expectimax(child_env, agent_id, tree_height - 1, (turn+1)%2)
+                    else:
+                        val = (1/(num_ops+2)) * self.RB_expectimax(child_env, agent_id, tree_height - 1, (turn+1)%2)
+                if ("move east" in child_ops) or ("pick up" in child_ops):
+                    if (op is "move east") or (op is "pick up"):
+                        val = (2/(num_ops+1)) * self.RB_expectimax(child_env, agent_id, tree_height - 1, (turn+1)%2)
+                    else:
+                        val = (1/(num_ops+1)) * self.RB_expectimax(child_env, agent_id, tree_height - 1, (turn+1)%2)
+                else:
+                    val = (1/(num_ops)) * self.RB_expectimax(child_env, agent_id, tree_height - 1, (turn+1)%2)
+                sum_val = sum_val + val
+            return sum_val
+
+
+
+
+
+
+
+
 
 
 # here you can check specific paths to get to know the environment
